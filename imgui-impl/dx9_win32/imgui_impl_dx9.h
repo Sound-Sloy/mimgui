c0aa84ab9863ceb9e44910193c8e91ea8c76f4b9
Horea Tise <107001664+Sound-Sloy@users.noreply.github.com>
2024-07-17 21:26:48 +0300

    Upgrade to imgui 1.90.9

--- imgui-impl/dx9_win32/imgui_impl_dx9.h
+++ imgui-impl/dx9_win32/imgui_impl_dx9.h
@@ -1,36 +1,31 @@
-// ImGui Renderer for: DirectX9
-// This needs to be used along with a Platform Binding (e.g. Win32)
+// dear imgui: Renderer Backend for DirectX9
+// This needs to be used along with a Platform Backend (e.g. Win32)
 
 // Implemented features:
-//  [X] Renderer: User texture binding. Use 'LPDIRECT3DTEXTURE9' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
-//  [X] Renderer: Support for large meshes (64k+ vertices) with 16-bits indices.
+//  [X] Renderer: User texture binding. Use 'LPDIRECT3DTEXTURE9' as ImTextureID. Read the FAQ about ImTextureID!
+//  [X] Renderer: Large meshes support (64k+ vertices) with 16-bit indices.
 
-// You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
+// You can use unmodified imgui_impl_* files in your project. See examples/ folder for examples of using this.
+// Prefer including the entire imgui/ repository into your project (either as a copy or as a submodule), and only build the backends you need.
+// Learn about Dear ImGui:
+// - FAQ                  https://dearimgui.com/faq
+// - Getting Started      https://dearimgui.com/getting-started
+// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
+// - Introduction, links and more at the top of imgui.cpp
 
-#include <d3d9.h>
+#pragma once
+#include "imgui.h"      // IMGUI_IMPL_API
+#ifndef IMGUI_DISABLE
 
-struct ImGui_ImplDX9_Context
-{
-	LPDIRECT3DDEVICE9        pd3dDevice;
-	LPDIRECT3DVERTEXBUFFER9  pVB;
-	LPDIRECT3DINDEXBUFFER9   pIB;
-	LPDIRECT3DTEXTURE9       FontTexture;
-	int                      VertexBufferSize;
-	int                      IndexBufferSize;
-};
+struct IDirect3DDevice9;
 
-IMGUI_IMPL_API ImGui_ImplDX9_Context* ImGui_ImplDX9_Init(LPDIRECT3DDEVICE9 device);
-IMGUI_IMPL_API void ImGui_ImplDX9_Shutdown(ImGui_ImplDX9_Context* context);
-IMGUI_IMPL_API void ImGui_ImplDX9_NewFrame(ImGui_ImplDX9_Context* context);
-IMGUI_IMPL_API void ImGui_ImplDX9_RenderDrawData(ImGui_ImplDX9_Context* context, ImDrawData* draw_data);
+IMGUI_IMPL_API bool     ImGui_ImplDX9_Init(IDirect3DDevice9* device);
+IMGUI_IMPL_API void     ImGui_ImplDX9_Shutdown();
+IMGUI_IMPL_API void     ImGui_ImplDX9_NewFrame();
+IMGUI_IMPL_API void     ImGui_ImplDX9_RenderDrawData(ImDrawData* draw_data);
 
-// Use if you want to reset your rendering device without losing ImGui state.
-IMGUI_IMPL_API void ImGui_ImplDX9_InvalidateDeviceObjects(ImGui_ImplDX9_Context* context);
-IMGUI_IMPL_API bool ImGui_ImplDX9_CreateFontsTexture(ImGui_ImplDX9_Context* context); // replaces ImGui_ImplDX9_CreateDeviceObjects since they are the same
-IMGUI_IMPL_API void ImGui_ImplDX9_InvalidateFontsTexture(ImGui_ImplDX9_Context* context);
+// Use if you want to reset your rendering device without losing Dear ImGui state.
+IMGUI_IMPL_API bool     ImGui_ImplDX9_CreateDeviceObjects();
+IMGUI_IMPL_API void     ImGui_ImplDX9_InvalidateDeviceObjects();
 
-IMGUI_IMPL_API LPDIRECT3DTEXTURE9 ImGui_ImplDX9_CreateTextureFromFile(LPDIRECT3DDEVICE9 device, LPCTSTR path);
-IMGUI_IMPL_API LPDIRECT3DTEXTURE9 ImGui_ImplDX9_CreateTextureFromFileInMemory(LPDIRECT3DDEVICE9 device, LPCVOID src, UINT size);
-IMGUI_IMPL_API void ImGui_ImplDX9_ReleaseTexture(LPDIRECT3DTEXTURE9 tex);
+#endif // #ifndef IMGUI_DISABLE
